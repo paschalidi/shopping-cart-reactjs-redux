3dde7d6a5ed70c23cc8f86c2259bf36d5f35e503
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-03-13 01:22:01 +0200

    master: updates component_card so tha it handles every possible action

--- src/components/component_card.js
+++ src/components/component_card.js
@@ -14,11 +14,51 @@ class CardComponent extends Component {
       description: this.props.description,
       itemsUntilDiscount: this.props.itemsUntilDiscount,
       discount: this.props.discount,
+
+      timesItemIsInCart: 0,
+      isDiscounted: false,
+      priceAfterDiscount: this.props.price - parseInt((this.props.discount / 100) * this.props.price)
     }
   }
 
+  updateState() {
+    this.setState({ timesItemIsInCart: this.state.timesItemIsInCart + 1 });
+
+    if (this.state.itemsUntilDiscount > 0)
+      this.setState({ itemsUntilDiscount: this.state.itemsUntilDiscount - 1 });
+
+    if (this.state.itemsUntilDiscount === 1)
+      this.setState({ isDiscounted: true });
+  }
+
   handleItemAddition() {
+    this.updateState();
+
     this.props.addItemToCart(this.state.title);
+    this.props.calculateItemsQuantity();
+    this.props.calculateTotalPrices(this.state.price, this.state.priceAfterDiscount, this.state.isDiscounted)
+  }
+
+  renderPriceAndDiscountMessage() {
+    if (this.state.isDiscounted) {
+      return (
+        <div>
+          <p>Discounted Price : from {this.state.price}$ to {this.state.priceAfterDiscount}$</p>
+          <p>You are getting {this.state.discount}% for every item you buy! Awesome!</p>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          <p>Price: {this.state.price} $</p>
+          <p>
+            Buy {this.state.itemsUntilDiscount} more and GET {this.state.discount}% DISCOUNT for
+            the next items of this kind!
+          </p>
+        </div>
+      )
+    }
   }
 
   render() {
@@ -30,7 +70,7 @@ class CardComponent extends Component {
             <div className="caption">
               <h4>{this.state.title}</h4>
               <p>{this.state.description}</p>
-              <p>Price: {this.state.price}</p>
+              {this.renderPriceAndDiscountMessage()}
               <button className="btn btn-success bottom-left"
                       onClick={this.handleItemAddition.bind(this)}>
                 Add robot to Cart
