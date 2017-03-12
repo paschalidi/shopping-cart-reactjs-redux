fe61e713e6ab983c6f4c605a411d92873117b727
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-03-13 01:23:11 +0200

    master: implemets reducers for the *CART_ITEM_COUNTER and *TOTAL_PRICE

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,31 @@
-import { ADD_ITEM_TO_CART } from './types';
+import {
+  ADD_ITEM_TO_CART,
+  CART_ITEM_COUNTER,
+  TOTAL_PRICE
+} from './types';
 
 export function addItemToCart(title) {
   return {
     type: ADD_ITEM_TO_CART,
     payload: { title }
+  };
+}
+
+export function calculateItemsQuantity() {
+  return {
+    type: CART_ITEM_COUNTER,
+  };
+}
+
+export function calculateTotalPrices(price, priceAfterDiscount, isDiscounted) {
+  if (!isDiscounted)
+    priceAfterDiscount = price;
+
+  return {
+    type: TOTAL_PRICE,
+    payload: {
+      totalPrice: price,
+      discountedPrice: priceAfterDiscount
+    }
   };
 }
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,3 @@
 export const ADD_ITEM_TO_CART = 'add_item_to_cart';
+export const CART_ITEM_COUNTER = 'count_items';
+export const TOTAL_PRICE = 'total_price';
