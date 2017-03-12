84c4991e71a1c8532ce419043dde70e2aacbad10
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-03-12 22:29:17 +0200

    master: creates dummy-data into reducer to work with
    data structure of dummy data and details would be in the comments of this commit

---
+++ src/reducers/dummy_items_data.js
@@ -0,0 +1,403 @@
+export default function () {
+  return [{
+    "id": 1,
+    "title": "Paralegal",
+    "image": "https://robohash.org/ducimuspariaturassumenda.png?size=200x200&set=set1",
+    "price": 142,
+    "description": "target user-centric relationships",
+    "itemsUntilDiscount": 7,
+    "discount": 42
+  }, {
+    "id": 2,
+    "title": "Electrical Engineer",
+    "image": "https://robohash.org/amolestiaeeum.png?size=200x200&set=set1",
+    "price": 185,
+    "description": "monetize efficient metrics",
+    "itemsUntilDiscount": 3,
+    "discount": 19
+  }, {
+    "id": 3,
+    "title": "Staff Scientist",
+    "image": "https://robohash.org/quasistecommodi.png?size=200x200&set=set1",
+    "price": 140,
+    "description": "unleash robust content",
+    "itemsUntilDiscount": 8,
+    "discount": 10
+  }, {
+    "id": 4,
+    "title": "Assistant Manager",
+    "image": "https://robohash.org/doloremqueoditiure.png?size=200x200&set=set1",
+    "price": 163,
+    "description": "deploy world-class metrics",
+    "itemsUntilDiscount": 9,
+    "discount": 15
+  }, {
+    "id": 5,
+    "title": "Director of Sales",
+    "image": "https://robohash.org/velveniamqui.png?size=200x200&set=set1",
+    "price": 136,
+    "description": "benchmark open-source convergence",
+    "itemsUntilDiscount": 6,
+    "discount": 43
+  }, {
+    "id": 6,
+    "title": "Marketing Assistant",
+    "image": "https://robohash.org/doloremveroillum.png?size=200x200&set=set1",
+    "price": 147,
+    "description": "harness scalable methodologies",
+    "itemsUntilDiscount": 10,
+    "discount": 23
+  }, {
+    "id": 7,
+    "title": "Teacher",
+    "image": "https://robohash.org/minimaquialibero.png?size=200x200&set=set1",
+    "price": 176,
+    "description": "streamline global vortals",
+    "itemsUntilDiscount": 4,
+    "discount": 13
+  }, {
+    "id": 8,
+    "title": "Payment Adjustment Coordinator",
+    "image": "https://robohash.org/quasiutat.png?size=200x200&set=set1",
+    "price": 168,
+    "description": "reintermediate value-added relationships",
+    "itemsUntilDiscount": 8,
+    "discount": 30
+  }, {
+    "id": 9,
+    "title": "GIS Technical Architect",
+    "image": "https://robohash.org/voluptateveritatisnon.png?size=200x200&set=set1",
+    "price": 94,
+    "description": "target killer portals",
+    "itemsUntilDiscount": 10,
+    "discount": 44
+  }, {
+    "id": 10,
+    "title": "Financial Advisor",
+    "image": "https://robohash.org/fugitrerumea.png?size=200x200&set=set1",
+    "price": 191,
+    "description": "redefine strategic solutions",
+    "itemsUntilDiscount": 7,
+    "discount": 30
+  }, {
+    "id": 11,
+    "title": "Librarian",
+    "image": "https://robohash.org/voluptatemidut.png?size=200x200&set=set1",
+    "price": 153,
+    "description": "transition robust mindshare",
+    "itemsUntilDiscount": 2,
+    "discount": 39
+  }, {
+    "id": 12,
+    "title": "Senior Editor",
+    "image": "https://robohash.org/quodvoluptasblanditiis.png?size=200x200&set=set1",
+    "price": 115,
+    "description": "innovate B2B bandwidth",
+    "itemsUntilDiscount": 6,
+    "discount": 20
+  }, {
+    "id": 13,
+    "title": "Chief Design Engineer",
+    "image": "https://robohash.org/atrerumpraesentium.png?size=200x200&set=set1",
+    "price": 132,
+    "description": "morph integrated metrics",
+    "itemsUntilDiscount": 9,
+    "discount": 13
+  }, {
+    "id": 14,
+    "title": "Compensation Analyst",
+    "image": "https://robohash.org/accusamussuscipitrepellat.png?size=200x200&set=set1",
+    "price": 161,
+    "description": "incubate global eyeballs",
+    "itemsUntilDiscount": 10,
+    "discount": 42
+  }, {
+    "id": 15,
+    "title": "Senior Editor",
+    "image": "https://robohash.org/debitisdoloribuset.png?size=200x200&set=set1",
+    "price": 176,
+    "description": "incentivize bricks-and-clicks action-items",
+    "itemsUntilDiscount": 4,
+    "discount": 18
+  }, {
+    "id": 16,
+    "title": "Electrical Engineer",
+    "image": "https://robohash.org/modiutdebitis.png?size=200x200&set=set1",
+    "price": 190,
+    "description": "streamline killer convergence",
+    "itemsUntilDiscount": 7,
+    "discount": 49
+  }, {
+    "id": 17,
+    "title": "Safety Technician IV",
+    "image": "https://robohash.org/quiaetsoluta.png?size=200x200&set=set1",
+    "price": 55,
+    "description": "generate killer deliverables",
+    "itemsUntilDiscount": 5,
+    "discount": 34
+  }, {
+    "id": 18,
+    "title": "Food Chemist",
+    "image": "https://robohash.org/remautsed.png?size=200x200&set=set1",
+    "price": 185,
+    "description": "iterate front-end partnerships",
+    "itemsUntilDiscount": 9,
+    "discount": 21
+  }, {
+    "id": 19,
+    "title": "Structural Engineer",
+    "image": "https://robohash.org/repellendusmolestiasodio.png?size=200x200&set=set1",
+    "price": 69,
+    "description": "expedite visionary e-business",
+    "itemsUntilDiscount": 2,
+    "discount": 31
+  }, {
+    "id": 20,
+    "title": "Librarian",
+    "image": "https://robohash.org/providentnonvelit.png?size=200x200&set=set1",
+    "price": 106,
+    "description": "transition 24/365 networks",
+    "itemsUntilDiscount": 4,
+    "discount": 40
+  }, {
+    "id": 21,
+    "title": "Speech Pathologist",
+    "image": "https://robohash.org/cupiditatererumet.png?size=200x200&set=set1",
+    "price": 123,
+    "description": "deliver distributed e-commerce",
+    "itemsUntilDiscount": 7,
+    "discount": 24
+  }, {
+    "id": 22,
+    "title": "VP Quality Control",
+    "image": "https://robohash.org/eumdeseruntiste.png?size=200x200&set=set1",
+    "price": 140,
+    "description": "incubate bleeding-edge eyeballs",
+    "itemsUntilDiscount": 8,
+    "discount": 18
+  }, {
+    "id": 23,
+    "title": "Help Desk Operator",
+    "image": "https://robohash.org/nequehicut.png?size=200x200&set=set1",
+    "price": 98,
+    "description": "architect holistic schemas",
+    "itemsUntilDiscount": 6,
+    "discount": 29
+  }, {
+    "id": 24,
+    "title": "Chief Design Engineer",
+    "image": "https://robohash.org/estdoloremest.png?size=200x200&set=set1",
+    "price": 150,
+    "description": "grow world-class platforms",
+    "itemsUntilDiscount": 7,
+    "discount": 13
+  }, {
+    "id": 25,
+    "title": "Budget/Accounting Analyst I",
+    "image": "https://robohash.org/omnisautdolor.png?size=200x200&set=set1",
+    "price": 124,
+    "description": "innovate wireless solutions",
+    "itemsUntilDiscount": 4,
+    "discount": 15
+  }, {
+    "id": 26,
+    "title": "Research Assistant I",
+    "image": "https://robohash.org/reprehenderitnonest.png?size=200x200&set=set1",
+    "price": 155,
+    "description": "synergize intuitive technologies",
+    "itemsUntilDiscount": 9,
+    "discount": 24
+  }, {
+    "id": 27,
+    "title": "Media Manager II",
+    "image": "https://robohash.org/autexcepturitemporibus.png?size=200x200&set=set1",
+    "price": 168,
+    "description": "brand dynamic e-services",
+    "itemsUntilDiscount": 6,
+    "discount": 20
+  }, {
+    "id": 28,
+    "title": "Staff Scientist",
+    "image": "https://robohash.org/nemoatvoluptatem.png?size=200x200&set=set1",
+    "price": 170,
+    "description": "extend plug-and-play action-items",
+    "itemsUntilDiscount": 4,
+    "discount": 50
+  }, {
+    "id": 29,
+    "title": "Assistant Professor",
+    "image": "https://robohash.org/nostrumevenietaperiam.png?size=200x200&set=set1",
+    "price": 164,
+    "description": "expedite back-end e-commerce",
+    "itemsUntilDiscount": 6,
+    "discount": 28
+  }, {
+    "id": 30,
+    "title": "Budget/Accounting Analyst III",
+    "image": "https://robohash.org/evenietrepellatnulla.png?size=200x200&set=set1",
+    "price": 102,
+    "description": "optimize dynamic mindshare",
+    "itemsUntilDiscount": 9,
+    "discount": 50
+  }, {
+    "id": 31,
+    "title": "General Manager",
+    "image": "https://robohash.org/minusculpasunt.png?size=200x200&set=set1",
+    "price": 143,
+    "description": "deliver next-generation schemas",
+    "itemsUntilDiscount": 6,
+    "discount": 29
+  }, {
+    "id": 32,
+    "title": "VP Accounting",
+    "image": "https://robohash.org/voluptasdoloresearum.png?size=200x200&set=set1",
+    "price": 81,
+    "description": "optimize web-enabled action-items",
+    "itemsUntilDiscount": 5,
+    "discount": 14
+  }, {
+    "id": 33,
+    "title": "Computer Systems Analyst IV",
+    "image": "https://robohash.org/consequaturutullam.png?size=200x200&set=set1",
+    "price": 50,
+    "description": "deploy global eyeballs",
+    "itemsUntilDiscount": 2,
+    "discount": 13
+  }, {
+    "id": 34,
+    "title": "Civil Engineer",
+    "image": "https://robohash.org/aliquamsuscipita.png?size=200x200&set=set1",
+    "price": 106,
+    "description": "optimize web-enabled action-items",
+    "itemsUntilDiscount": 4,
+    "discount": 28
+  }, {
+    "id": 35,
+    "title": "Junior Executive",
+    "image": "https://robohash.org/idipsaexplicabo.png?size=200x200&set=set1",
+    "price": 146,
+    "description": "morph wireless users",
+    "itemsUntilDiscount": 4,
+    "discount": 25
+  }, {
+    "id": 36,
+    "title": "Graphic Designer",
+    "image": "https://robohash.org/utquivoluptatem.png?size=200x200&set=set1",
+    "price": 76,
+    "description": "transform 24/7 solutions",
+    "itemsUntilDiscount": 3,
+    "discount": 40
+  }, {
+    "id": 37,
+    "title": "GIS Technical Architect",
+    "image": "https://robohash.org/voluptaseaquesequi.png?size=200x200&set=set1",
+    "price": 60,
+    "description": "revolutionize cross-platform portals",
+    "itemsUntilDiscount": 3,
+    "discount": 34
+  }, {
+    "id": 38,
+    "title": "Technical Writer",
+    "image": "https://robohash.org/aliquamautemet.png?size=200x200&set=set1",
+    "price": 53,
+    "description": "repurpose turn-key models",
+    "itemsUntilDiscount": 8,
+    "discount": 48
+  }, {
+    "id": 39,
+    "title": "Data Coordiator",
+    "image": "https://robohash.org/eaaccusantiumrerum.png?size=200x200&set=set1",
+    "price": 140,
+    "description": "benchmark wireless methodologies",
+    "itemsUntilDiscount": 6,
+    "discount": 31
+  }, {
+    "id": 40,
+    "title": "Design Engineer",
+    "image": "https://robohash.org/quasiminusfacere.png?size=200x200&set=set1",
+    "price": 74,
+    "description": "syndicate revolutionary markets",
+    "itemsUntilDiscount": 3,
+    "discount": 49
+  }, {
+    "id": 41,
+    "title": "Nurse Practicioner",
+    "image": "https://robohash.org/hicsedconsectetur.png?size=200x200&set=set1",
+    "price": 96,
+    "description": "harness B2B communities",
+    "itemsUntilDiscount": 7,
+    "discount": 32
+  }, {
+    "id": 42,
+    "title": "Account Coordinator",
+    "image": "https://robohash.org/estdoloremquefacere.png?size=200x200&set=set1",
+    "price": 150,
+    "description": "harness transparent communities",
+    "itemsUntilDiscount": 3,
+    "discount": 36
+  }, {
+    "id": 43,
+    "title": "Environmental Tech",
+    "image": "https://robohash.org/nihilbeataevoluptatibus.png?size=200x200&set=set1",
+    "price": 175,
+    "description": "harness cross-media bandwidth",
+    "itemsUntilDiscount": 4,
+    "discount": 46
+  }, {
+    "id": 44,
+    "title": "Executive Secretary",
+    "image": "https://robohash.org/sequivoluptatemsaepe.png?size=200x200&set=set1",
+    "price": 136,
+    "description": "exploit customized channels",
+    "itemsUntilDiscount": 2,
+    "discount": 43
+  }, {
+    "id": 45,
+    "title": "Senior Financial Analyst",
+    "image": "https://robohash.org/evenietdolordolore.png?size=200x200&set=set1",
+    "price": 123,
+    "description": "generate innovative paradigms",
+    "itemsUntilDiscount": 2,
+    "discount": 37
+  }, {
+    "id": 46,
+    "title": "Environmental Tech",
+    "image": "https://robohash.org/voluptateconsequaturcumque.png?size=200x200&set=set1",
+    "price": 197,
+    "description": "utilize extensible infomediaries",
+    "itemsUntilDiscount": 5,
+    "discount": 20
+  }, {
+    "id": 47,
+    "title": "Cost Accountant",
+    "image": "https://robohash.org/quiestomnis.png?size=200x200&set=set1",
+    "price": 150,
+    "description": "enable open-source convergence",
+    "itemsUntilDiscount": 6,
+    "discount": 10
+  }, {
+    "id": 48,
+    "title": "Assistant Professor",
+    "image": "https://robohash.org/earumlaboriosamet.png?size=200x200&set=set1",
+    "price": 79,
+    "description": "morph cross-media communities",
+    "itemsUntilDiscount": 3,
+    "discount": 37
+  }, {
+    "id": 49,
+    "title": "Senior Sales Associate",
+    "image": "https://robohash.org/occaecatiataperiam.png?size=200x200&set=set1",
+    "price": 80,
+    "description": "architect compelling initiatives",
+    "itemsUntilDiscount": 7,
+    "discount": 26
+  }, {
+    "id": 50,
+    "title": "Quality Control Specialist",
+    "image": "https://robohash.org/atrerumdolor.png?size=200x200&set=set1",
+    "price": 152,
+    "description": "syndicate proactive infrastructures",
+    "itemsUntilDiscount": 3,
+    "discount": 44
+  }]
+}
