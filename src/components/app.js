3d7c5b592771cc8398514b3806b6e7fdd73927a9
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-03-12 22:51:34 +0200

    master: wires up the component_content into the app

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import NavigationBar from './component_navbar'
+import Content from './component_content'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavigationBar />
+        <Content />
       </div>
     );
   }
