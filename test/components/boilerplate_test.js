531f033f6f6bbf8d68cf6e0019fea969974e9a2e
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-03-12 22:47:16 +0200

    master: creates a boilerplate/skeleton for react components testing

---
+++ test/components/boilerplate_test.js
@@ -0,0 +1,23 @@
+import { renderComponent, expect } from '../test_helper';
+// import AComponent from '../../src/components/';
+
+describe('AComponent', () => {
+  let component;
+  beforeEach(() => {
+    component = renderComponent(AComponent);
+  });
+
+  describe('contains child components', () => {
+  });
+
+  describe('html, style and text', () => {
+    describe('must have class', () => {
+    });
+
+    describe('must have element', () => {
+    });
+
+    describe('must have text', () => {
+    })
+  });
+});
