bcc261634d832fde55bafc584caf8550a8d44764
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-03-13 01:25:20 +0200

    master: writes tests for action creators

---
+++ test/actions/boilerplate_test.js
@@ -0,0 +1,12 @@
+import { expect } from '../test_helper'
+import {} from '../../src/actions/types'
+import {} from '../../src/actions/index'
+
+describe('actions', () => {
+  describe('an action', () => {
+    // it('must have correct type', () => {
+    // });
+    // it('must have correct payload', () => {
+    // })
+  })
+})
---
+++ test/actions/index_test.js
@@ -0,0 +1,52 @@
+import { expect } from '../test_helper'
+import {
+  ADD_ITEM_TO_CART,
+  CART_ITEM_COUNTER,
+  TOTAL_PRICE
+} from '../../src/actions/types'
+import {
+  addItemToCart,
+  calculateItemsQuantity,
+  calculateTotalPrices
+} from '../../src/actions/index'
+
+describe('Actions', () => {
+
+  describe('addItemToCart', () => {
+    it('must have correct type', () => {
+      const action = addItemToCart();
+      expect(action.type).to.equal(ADD_ITEM_TO_CART)
+    });
+    it('must have correct payload', () => {
+      const action = addItemToCart('a new title');
+      expect(action.payload).to.eql({ title: 'a new title' })
+    });
+  });
+
+  describe('calculateItemQuantity', () => {
+    it('must have correct type', () => {
+      const action = calculateItemsQuantity();
+      expect(action.type).to.equal(CART_ITEM_COUNTER)
+    });
+  });
+
+  describe('calculateTotalPrices', () => {
+    it('must have correct type', () => {
+      const action = calculateTotalPrices();
+      expect(action.type).to.eql(TOTAL_PRICE)
+    });
+    it('must have correct payload', () => {
+      let action;
+      action =calculateTotalPrices(50, 40, false);
+      expect(action.payload).to.eql({
+        totalPrice: 50,
+        discountedPrice: 50
+      });
+      action = calculateTotalPrices(50, 40, true);
+      expect(action.payload).to.eql({
+        totalPrice: 50,
+        discountedPrice: 40
+      })
+    });
+  });
+});
