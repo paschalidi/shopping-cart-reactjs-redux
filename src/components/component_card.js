5b7f331270d394413782a6549e9e0c1551c18abc
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-03-17 01:59:01 +0200

    invalid_markup: fixes invalid markup

--- src/components/component_card.js
+++ src/components/component_card.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import * as actions from '../actions';
+import * as actions from '../actions/index';
 
 class CardComponent extends Component {
   constructor(props) {
