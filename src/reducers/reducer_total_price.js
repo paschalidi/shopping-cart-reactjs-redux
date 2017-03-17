63b41b88326c45fbdd557ad0195eb28c7c42095e
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-03-17 02:25:36 +0200

    refactoring: moves logic of the application to reducer level

--- src/reducers/reducer_total_price.js
+++ src/reducers/reducer_total_price.js
@@ -5,9 +5,12 @@ const INITIAL_STATE = { totalPrice: 0, totalDiscountedPrice: 0 };
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case TOTAL_PRICE:
+      let priceAfterDiscount = (action.payload.isDiscounted) ?
+          action.payload.priceAfterDiscount : action.payload.price;
+
       return {
-        totalPrice: state.totalPrice + action.payload.totalPrice,
-        totalDiscountedPrice: state.totalDiscountedPrice + action.payload.discountedPrice
+        totalPrice: state.totalPrice + action.payload.price,
+        totalDiscountedPrice: state.totalDiscountedPrice + priceAfterDiscount
       };
   }
   return state
